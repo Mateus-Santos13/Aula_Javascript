39cb4bb63942dd7d7ca295901706c2798f2ee5ea
Mateus-Santos13 <mateus.santos.vieira.eng.elt@gmail.com>
2026-01-27 10:36:55 -0300

    Atualização do código

--- Aula_02/Variaveis.js
+++ Aula_02/Variaveis.js
@@ -1,4 +1,4 @@
-let nome;
+let nome = "Mateus Santos";
 let profissao = "Pessoa Desenvolvedora";
 let salarioBruto = 3500.90;
 const bonus = 0.05;
@@ -8,4 +8,13 @@ console.log("O tipo da variável nome é: " + typeof (nome));
 
 if (salarioBruto > 1000){
     var mensagem = "Salario > 1000";
+    console.log(mensagem);
 }
+
+
+console.log("Salário líquido:" ,
+
+    new Intl.NumberFormat('us-US', { 
+        style: 'currency', currency: 'USA' 
+    }).format(salarioBruto + (salarioBruto * bonus)) //Tentar quebrar as linhas de código para facilitar leitura e correção dos códigos
+);
