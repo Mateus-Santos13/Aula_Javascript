732c6495a105fc5a077515b4209ced289952ac67
Mateus-Santos13 <mateus.santos.vieira.eng.elt@gmail.com>
2026-01-27 09:49:56 -0300

    Aula 02 de javascript pelo curso da Generation Brasil

---
+++ Aula_02/Variaveis.js
@@ -0,0 +1,11 @@
+let nome;
+let profissao = "Pessoa Desenvolvedora";
+let salarioBruto = 3500.90;
+const bonus = 0.05;
+
+
+console.log("O tipo da variável nome é: " + typeof (nome));
+
+if (salarioBruto > 1000){
+    var mensagem = "Salario > 1000";
+}
